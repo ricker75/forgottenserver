21d18f00d73321e8e1595087d671d2dda78da5ab
brunominervino <brunominervino1993@gmail.com>
2015-05-18 22:54:16 -0300

    Compatibility to Version 10.78

--- src/definitions.h
+++ src/definitions.h
@@ -25,8 +25,8 @@
 #define STATUS_SERVER_DEVELOPERS "Mark Samman and edited by Bruno Minervino"
 
 #define CLIENT_VERSION_MIN 1076
-#define CLIENT_VERSION_MAX 1077
-#define CLIENT_VERSION_STR "10.77"
+#define CLIENT_VERSION_MAX 1078
+#define CLIENT_VERSION_STR "10.78"
 
 #ifndef __FUNCTION__
 #define __FUNCTION__ __func__
